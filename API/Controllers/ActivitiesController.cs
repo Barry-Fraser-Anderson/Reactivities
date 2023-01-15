eb26ae8739a29c1b054a4db23978ee40960105a6
Barry Anderson <barryanderson60@outlook.com>
2023-01-15 22:21:24 +0000

    Edit handler now uses AutoMapper

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -25,12 +25,10 @@ namespace API.Controllers
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult<Activity>> EditActivity(Guid id, Activity activity)
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
             return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
-
-
     }
 }
