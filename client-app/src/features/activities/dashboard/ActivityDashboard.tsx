6a7e09aa8a30901a46c999301c1910d8fe34b75b
Barry Anderson <barryanderson60@outlook.com>
2023-01-18 23:02:15 +0000

    Post data to the server

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Grid } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
-import ActivityList from "./ActivityList";
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
+import ActivityList from './ActivityList';
 
 interface Props {
     activities: Activity[];
@@ -15,6 +15,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({
@@ -27,6 +28,7 @@ export default function ActivityDashboard({
     closeForm,
     createOrEdit,
     deleteActivity,
+    submitting,
 }: Props) {
     return (
         <Grid>
@@ -50,6 +52,7 @@ export default function ActivityDashboard({
                         closeForm={closeForm}
                         activity={selectedActivity}
                         createOrEdit={createOrEdit}
+                        submitting={submitting}
                     />
                 )}
             </Grid.Column>
