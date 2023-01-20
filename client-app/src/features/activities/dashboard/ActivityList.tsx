26c013971331290d7a5dfeac91e55a5bfd24c9ba
Barry Anderson <barryanderson60@outlook.com>
2023-01-20 20:48:57 +0000

    Creating an Activity using MobX

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,25 +1,22 @@
 import React, { SyntheticEvent, useState } from 'react';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
+import { useStore } from '../../../app/stores/store';
 
 interface Props {
     activities: Activity[];
-    selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
     submitting: boolean;
 }
-export default function ActivityList({
-    activities,
-    selectActivity,
-    deleteActivity,
-    submitting,
-}: Props) {
+export default function ActivityList({ activities, deleteActivity, submitting }: Props) {
     const [target, setTarget] = useState('');
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
         setTarget(e.currentTarget.name);
         deleteActivity(id);
     }
+
+    const { activityStore } = useStore();
 
     return (
         <Segment>
@@ -37,7 +34,7 @@ export default function ActivityList({
                             </Item.Description>
                             <Item.Extra>
                                 <Button
-                                    onClick={() => selectActivity(activity.id)}
+                                    onClick={() => activityStore.selectActivity(activity.id)}
                                     floated='right'
                                     content='View'
                                     color='blue'
