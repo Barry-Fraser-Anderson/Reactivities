357bf0624494b6614d4d6228040210b4a2df24ae
Barry Anderson <barryanderson60@outlook.com>
2023-01-23 20:32:20 +0000

    Form submission with Formik

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,13 @@
 import { observer } from 'mobx-react-lite';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, Header, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
+import { v4 as uuid } from 'uuid';
 import MyTextInput from '../../../app/common/form/MyTextInput';
 import MyTextArea from '../../../app/common/form/MyTextArea';
 import MySelectInput from '../../../app/common/form/MySelectInput';
@@ -33,7 +34,7 @@ export default observer(function ActivityForm() {
         title: Yup.string().required('The activity title is required'),
         description: Yup.string().required('The activity description is required'),
         category: Yup.string().required(),
-        date: Yup.string().required(),
+        date: Yup.string().required('Date is required').nullable(),
         venue: Yup.string().required(),
         city: Yup.string().required(),
     });
@@ -42,31 +43,30 @@ export default observer(function ActivityForm() {
         if (id) loadActivity(id).then((activity) => setActivity(activity!));
     }, [id, loadActivity]);
 
-    // function handleSubmit() {
-    //     if (!activity.id) {
-    //         activity.id = uuid();
-    //         createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    //     } else {
-    //         updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    //     }
-    // }
-
-    // function handleChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-    //     const { name, value } = event.target;
-    //     setActivity({ ...activity, [name]: value });
-    // }
+    function handleFormSubmit(activity: Activity) {
+        if (activity.id.length === 0) {
+            let newActivity = {
+                ...activity,
+                id: uuid(),
+            };
+            createActivity(newActivity).then(() => navigate(`/activities/${newActivity.id}`));
+        } else {
+            updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+        }
+    }
 
     if (loadingInitial) return <LoadingComponent content='Loading activity...' />;
 
     return (
         <Segment clearing>
+            <Header content='Activity Details' sub color='teal' />
             <Formik
                 validationSchema={validationSchema}
                 enableReinitialize
                 initialValues={activity}
-                onSubmit={(values) => console.log(values)}
+                onSubmit={(values) => handleFormSubmit(values)}
             >
-                {({ handleSubmit }) => (
+                {({ handleSubmit, isValid, isSubmitting, dirty }) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                         <MyTextInput name='title' placeholder='Title' />
                         <MyTextArea rows={3} placeholder='Description' name='description' />
@@ -82,9 +82,11 @@ export default observer(function ActivityForm() {
                             timeCaption='time'
                             dateFormat='MMMM d, yyyy h:mm aa'
                         />
-                        <MyTextInput placeholder='city' name='city' />
+                        <Header content='Location Details' sub color='teal' />
+                        <MyTextInput placeholder='City' name='city' />
                         <MyTextInput placeholder='Venue' name='venue' />
                         <Button
+                            disabled={isSubmitting || !dirty || !isValid}
                             loading={loading}
                             floated='right'
                             positive
