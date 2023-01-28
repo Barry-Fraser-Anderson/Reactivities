dfc88485c5d26258b1a593c14bbeb08bffc8e2b3
Barry Anderson <barryanderson60@outlook.com>
2023-01-28 19:14:33 +0000

    Added attendees to activity list item

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -33,7 +33,7 @@ export default function ActivityListItem({ activity }: Props) {
                 </span>
             </Segment>
             <Segment secondary>
-                <ActivityListItemAttendee />
+                <ActivityListItemAttendee attendees={activity.attendees!} />
             </Segment>
             <Segment clearing>
                 <span>{activity.description}</span>
