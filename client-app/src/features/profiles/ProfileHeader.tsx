bececaf24848c0837e0e9af60892eaaa3eb5996d
Barry Anderson <barryanderson60@outlook.com>
2023-01-30 23:47:13 +0000

    Added profile header

---
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Grid, Header, Item, Segment, Statistic, Divider, Reveal, Button } from 'semantic-ui-react';
+
+export default function ProfileHeader() {
+    return (
+        <Segment>
+            <Grid>
+                <Grid.Column width={12}>
+                    <Item.Group>
+                        <Item>
+                            <Item.Image avatat size='small' src={'/assets/users.png'} />
+                            <Item.Content>
+                                <Header as='h1' content='Displayname' />
+                            </Item.Content>
+                        </Item>
+                    </Item.Group>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                    <Statistic.Group widths={2}>
+                        <Statistic label='Followers' value='5' />
+                        <Statistic label='Following' value='42' />
+                    </Statistic.Group>
+                    <Divider />
+                    <Reveal animated='move'>
+                        <Reveal.Content visible style={{ width: '100%' }}>
+                            <Button fluid color='teal' content='Following' />
+                        </Reveal.Content>
+                        <Reveal.Content hidden style={{ width: '100%' }}>
+                            <Button
+                                fluid
+                                basic
+                                color={true ? 'red' : 'green'}
+                                content={true ? 'Follow' : 'Unfollow'}
+                            />
+                        </Reveal.Content>
+                    </Reveal>
+                </Grid.Column>
+            </Grid>
+        </Segment>
+    );
+}
