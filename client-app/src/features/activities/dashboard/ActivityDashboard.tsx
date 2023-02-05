0b041146d8106016933960e1b2de56ab9ba94e79
Barry Anderson <barryanderson60@outlook.com>
2023-02-05 18:48:46 +0000

    Activity list placeholders

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,6 +7,7 @@ import { PagingParams } from '../../../app/models/pagination';
 import { useStore } from '../../../app/stores/store';
 import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
+import ActivityListItemPlaceholder from './ActivityListItemPlaceholder';
 
 export default observer(function ActivityDashboard() {
     const { activityStore } = useStore();
@@ -23,21 +24,25 @@ export default observer(function ActivityDashboard() {
         if (activityRegistry.size <= 1) loadActivities();
     }, [loadActivities, activityRegistry.size]);
 
-    if (activityStore.loadingInitial && !loadingNext)
-        return <LoadingComponent content='Loading activities' />;
-
     return (
         <Grid>
             <Grid.Column width='10'>
-                <InfiniteScroll
-                    pageStart={0}
-                    loadMore={handleGetNext}
-                    hasMore={
-                        !loadingNext &&
-                        !!pagination &&
-                        pagination.currentPage < pagination.totalPages
-                    }
-                    initialLoad={false}></InfiniteScroll>
+                {activityStore.loadingInitial && !loadingNext ? (
+                    <>
+                        <ActivityListItemPlaceholder />
+                        <ActivityListItemPlaceholder />
+                    </>
+                ) : (
+                    <InfiniteScroll
+                        pageStart={0}
+                        loadMore={handleGetNext}
+                        hasMore={
+                            !loadingNext &&
+                            !!pagination &&
+                            pagination.currentPage < pagination.totalPages
+                        }
+                        initialLoad={false}></InfiniteScroll>
+                )}
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
