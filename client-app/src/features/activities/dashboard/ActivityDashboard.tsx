0c92a20c29270e3f2ccbbab08ea3b88499344cb3
Barry Anderson <barryanderson60@outlook.com>
2023-02-05 15:07:41 +0000

    Infinite scrolling with react-infinite-scroller

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import React, { useEffect, useState } from 'react';
-import { Button, Grid } from 'semantic-ui-react';
+import InfiniteScroll from 'react-infinite-scroller';
+import { Grid, Loader } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { PagingParams } from '../../../app/models/pagination';
 import { useStore } from '../../../app/stores/store';
@@ -28,18 +29,22 @@ export default observer(function ActivityDashboard() {
     return (
         <Grid>
             <Grid.Column width='10'>
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={handleGetNext}
+                    hasMore={
+                        !loadingNext &&
+                        !!pagination &&
+                        pagination.currentPage < pagination.totalPages
+                    }
+                    initialLoad={false}></InfiniteScroll>
                 <ActivityList />
-                <Button
-                    floated='right'
-                    content='More...'
-                    positive
-                    onClick={handleGetNext}
-                    loading={loadingNext}
-                    disabled={pagination?.totalPages === pagination?.currentPage}
-                />
             </Grid.Column>
             <Grid.Column width='6'>
                 <ActivityFilters />
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Loader active={loadingNext} />
             </Grid.Column>
         </Grid>
     );
